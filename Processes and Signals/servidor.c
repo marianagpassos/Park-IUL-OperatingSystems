845e30c6e21f4c82cf2f4457922487a7e7a89ed4
marianagpassos <marianagpp5699@gmail.com>
2026-02-14 18:48:00 +0000

    Update servidor.c

--- Processes and Signals/servidor.c
+++ Processes and Signals/servidor.c
@@ -1,7 +1,7 @@
 /****************************************************************************************
  ** ISCTE-IUL: Trabalho prático 2 de Sistemas Operativos 2024/2025, Enunciado Versão 4+
  **
- **  Aluno: Nº:122660       Nome: Mariana Passos
+ **  Aluno: Nº:       Nome: Mariana Passos
  ** Nome do Módulo: servidor.c
  ** Descrição/Explicação do Módulo:
  **
